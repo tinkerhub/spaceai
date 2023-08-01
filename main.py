068118418378443c0d86c619d220fbec457de071
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2023-08-02 00:54:03 +0530

    fix an issue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
 from core.routes import router
-from core.telegram import check_telegram_webhook
 app = FastAPI()
 
 from dotenv import load_dotenv
@@ -12,5 +11,4 @@ load_dotenv("ops/.env")
 app.include_router(router.router)
 
 if __name__ == "__main__":
-    check_telegram_webhook()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
